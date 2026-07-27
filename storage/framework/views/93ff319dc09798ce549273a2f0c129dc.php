fae3682c5b7e526065e31eccbd4106c6da0cb1ed
RomanKaxo <romanek8888@email.cz>
2026-07-27 16:33:07 +0200

    .

--- storage/framework/views/93ff319dc09798ce549273a2f0c129dc.php
+++
@@ -1,5 +0,0 @@
-<div class="stats-card w-[272px] h-[100px] rounded-[8px] flex flex-col items-center justify-center bg-gradient-to-t from-[#FFFFFF] to-[#E6E6E6]">
-    <img src="<?php echo e($icon); ?>" class="w-[28px] h-[28px] mb-1" alt="<?php echo e($alt); ?>">
-    <span class="font-bold text-[24px] text-[#5C2D62]" style="font-family: 'Poppins', sans-serif;"><?php echo e($value); ?></span>
-    <span class="text-[13px] text-[#505050]" style="font-family: 'Poppins', sans-serif;"><?php echo e($label); ?></span>
-</div><?php /**PATH C:\Users\roman\Desktop\ZasukejSi\resources\views/components/dashboard/stats-card.blade.php ENDPATH**/ ?>
